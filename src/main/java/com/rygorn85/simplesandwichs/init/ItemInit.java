11e62d6d3594beb90a5afe12e849055e183c2c22
kgrover3 <kgroveriii@gmail.com>
2022-06-01 16:41:05 -0400

    Sandwich Knife added

---
+++ src/main/java/com/rygorn85/simplesandwichs/init/ItemInit.java
@@ -0,0 +1,30 @@
+package com.rygorn85.simplesandwichs.init;
+
+import com.rygorn85.simplesandwichs.SimpleSandwichsMain;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInit {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SimpleSandwichsMain.MOD_ID);
+
+    public static final RegistryObject<Item> SANDWICHKNIFE = ITEMS.register("sandwich_knife",
+            () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
+
+    public static class ModCreativeTab extends CreativeModeTab {
+        private ModCreativeTab(int index, String label) {
+            super(index, label);
+        }
+
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(SANDWICHKNIFE.get());
+        }
+
+        public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, "simplesandwichs");
+    }
+}
